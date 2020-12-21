b5370ebeedf08820d0156ba581553a6f6bc7a12a
josegzza <a01038061@itesm.mx>
2020-12-20 21:52:20 -0600

    adding dist subtree

---
+++ dist/js/AboutMe.cfb7717b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["AboutMe"],{"59e7":function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v(" hey baby ")])])}],c=t("2877"),l={},a=Object(c["a"])(l,r,u,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=AboutMe.cfb7717b.js.map
